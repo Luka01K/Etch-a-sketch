e5644ae3ac4ef352215b7dd151a93ee00f8480cc
Luka01K <147425539+Luka01K@users.noreply.github.com>
2024-02-29 02:25:37 +0100

    Finishing the page
    working on buttons, setting up color functions and click to draw

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
-document.addEventListener("DOMContentLoaded", function() {
-    createBoard(32)
+let color = "black"
 
+let click = false;
 
+document.addEventListener('DOMContentLoaded', function() {
+    createBoard(16);
+
+    document.querySelector('body').addEventListener('click', function(e) {
+        if (e.target.tagName != "BUTTON") {
+            click = !click;
+            let draw = document.querySelector("#draw");
+            if (click) {
+                draw.innerHTML = "The pen is down."
+            } else {
+                draw.innerHTML = "The pan is up."
+            }
+        }
+    })
+
+    let btnPopup = document.querySelector("#popup");
+    btnPopup.addEventListener('click', function () {
+        let size = getSize();
+        createBoard(size);
+    })
     console.log("heyo");
 })
 
@@ -16,8 +36,48 @@ function createBoard(size) {
 
     for (let i = 0; i < numDiv; i++) {
         let div = document.createElement('div');
-        div.style.backgroundColor = "#E6E3FA";
+        div.style.backgroundColor = "white";
+        div.addEventListener('mouseover', colorDiv)
+        
 
         board.insertAdjacentElement('beforeend', div);
     }
 }
+
+
+function getSize() {
+    let input = prompt("Enter the size of the board? (0 - 100)");
+    let message = document.querySelector("#message");
+
+    if (input == "") {
+        message.innerHTML = "Please provide a number."
+    } else if (input < 0 || input > 100) {
+        message.innerHTML = "Please provide a number between 0 and 100."
+    } else {
+        message.innerHTML = "Lets draw!"
+        return input;
+    }
+}
+
+
+function colorDiv() {
+    if (click) {
+        if (color == "random") {
+            this.style.backgroundColor = `hsl(${Math.random() *360}, 100%, 50%)`
+        } else {
+            this.style.backgroundColor = "black";
+        }
+    }
+}
+
+
+function setColor(colorChoice) {
+    color = colorChoice;
+}
+
+function resetBoard() {
+    let allDivs = document.querySelectorAll('div');
+    allDivs.forEach((div) => div.style.backgroundColor = "white")
+}
+
+
